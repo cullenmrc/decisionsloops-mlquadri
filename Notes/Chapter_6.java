a03bbf0a5027a1580575af554c73fdf56fdfa577
mlquadri <mlquadri@stu.naperville203.org>
2016-10-25 11:14:58 -0500

    took notes

--- Notes/Chapter_6.java
+++ Notes/Chapter_6.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Write a description of class Chapter_6 here.
  * 
@@ -68,5 +69,84 @@ public class Chapter_6
         {  
             System.out.println(i);
         }
+        }
+    public static void doExample()
+    {
+            /*
+         * do loop
+         *      exacutes the body of the loop
+         *      evaluats the boolean condition(like an if statment)
+         *          if true
+         *              continues loop
+         *          if false
+         *              continues after the loop
+         */
+        int count=1;
+        do
+        {
+            System.out.println(count);//body
+            
+            count +=1;//update
+        }
+        while (count <=5);//conditin
+    }
+    public static void sentinelExample()
+    {
+         /*
+         * sentinel Values
+         *      valess to termanate a while/do loop
+         *      entered by the user
+         */
+        
+        int sum=1;
+        Scanner s = new Scanner(System.in);
+        int value = 0;
+        do
+        {
+            System.out.println("enter an int(0 to quit)");//0 is the sentanal value
+            value = s.nextInt();
+            sum +=value;
+        }
+        while (value != 0);
+        System.out.println("sum"+sum);
+    }
+    public static void sentinelExample2()
+    {
+        int sum=1;
+        Scanner s = new Scanner(System.in);
+        int value = 0;
+        while(true)
+        {
+            System.out.println("enter an int(0 to quit)");//-1 is the sentanal value
+            value = s.nextInt();
+            if (value ==-1)
+            {
+                // only works on while and switches
+                break;
+            }
+            sum +=value;
+        }
+        System.out.println("sum"+sum);
+    }
+    public static void sentinelExample3()
+    {
+        int sum=1;
+        Scanner s = new Scanner(System.in);
+        int value = 0;
+        while(value!=-1)
+        {
+            System.out.println("enter an int(0 to quit)");//-1 is the sentanal value
+            value = s.nextInt();
+            if (value ==-1)
+            {
+                /*
+                 * skips to the end of the inter most loop
+                 * re evaluates condition
+                 */
+                continue;
+            }
+            sum +=value;
+        }
+        System.out.println("sum"+sum);
     }
 }
