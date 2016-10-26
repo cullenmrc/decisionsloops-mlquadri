975e704c5834febfd531939eb343b79498c15545
mlquadri <mlquadri@stu.naperville203.org>
2016-10-26 11:23:11 -0500

    worked on class labs

---
+++ CommonLoopAlgorithms/CommonLoopAlgorithms.java
@@ -0,0 +1,35 @@
+/**
+ * Write a description of class CommonLoopAlgorithms here.
+ * 
+ * @author (Table Green) 
+ * @version (10-26-16)
+ */
+public class CommonLoopAlgorithms
+{
+    /**
+     * Default constructor for objects of class CommonLoopAlgorithms
+     */
+    public CommonLoopAlgorithms()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
---
+++ Notes/Rewrite_Loops.java
@@ -0,0 +1,34 @@
+
+
+/**
+ * Rewrite the following for loop into a while loop.
+ * int s = 0;
+ * for (int i = 1; i <= 10; i++)
+ * {
+ *      s = s + i;
+ * }
+ * Rewrite the following do loop into a while loop.
+ * int n = in.nextInt();
+ * double x = 0;
+ * double s;
+ * do
+ * {
+ *      s = 1.0 / (1 + n * n);
+ *      n++;
+ *      x = x + s;
+ * }
+ * while (s > 0.01);
+ * 
+ * @author (Muhammed Quadri) 
+ * @version (10-26-16)
+ */
+public class Rewrite_Loops
+{
+    /**
+     * Default constructor for objects of class Rewrite_Loops
+     */
+    public Rewrite_Loops()
+    {
+        
+    }
+}
