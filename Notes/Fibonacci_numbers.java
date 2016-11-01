3b026fc87f0d8da8bfa09b8711447cd9fee591c6
mlquadri <mlquadri@stu.naperville203.org>
2016-11-01 11:26:12 -0500

    fsdgfsfd

--- Notes/Fibonacci_numbers.java
+++ Notes/Fibonacci_numbers.java
@@ -1,19 +1,42 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class Fibonacci_numbers
 {
     public static void main(String[] Args)
     {
-        int f1 = 1;
-        int f2 = 1;
-        int f3 = f1+f2;
-        Scanner s = new Scanner(System.in)
-        
-        while(true)
+        int p1 = 0;
+        int p2 = 0;
+        int f = 1;
+        int pos = -1;
+        Scanner s = new Scanner(System.in);
+        while(pos<0)
         {
-            f1 = f3;
-            f2 = f3;
-            f3 = f1+f2;
-            System.out.println(f3);
+            System.out.print("Enter the position of the Fibonacci Number(Enter 0 for the entire sequence): ");
+            pos = s.nextInt();
+            if (pos<0)
+            {
+                System.out.println("That is not a valid number");
+            }
+        }
+        if(pos==0)
+        {
+            int r=1;
+            while(r==2)
+            {
+                p1 = p2+0;
+                p2 = f+0;
+                f = p1+p2;
+                System.out.println(f);
+            }
+        }else{
+            for(int i=0;
+                i<=pos;
+                i++)
+            {
+                p1 = p2+0;
+                p2 = f+0;
+                f = p1+p2;
+            }
+            System.out.println(f);
         }
     }
 }
--- Notes/Substrings.java
+++ Notes/Substrings.java
@@ -19,40 +19,21 @@ public class Substrings
         Scanner s =  new Scanner(System.in);
         System.out.print("Enter String: ");
         String inp = s.next();
-        
-        
-        for ( int length = 1; length <= inp.length(); length++) {
-            
+        for ( int length = 1; length <= inp.length(); length++)
+        {
             for ( int startingpos = 0; startingpos < inp.length(); startingpos++) {
                 if(startingpos+length > inp.length() - 1) break;
                 System.out.println(inp.substring(startingpos,startingpos+length));
-                
             }
-            
         }
-        
         System.out.println(inp);
-            
-        
-        
-        
-        
-//         for( int run = 1;
-//                  run < inp.length();
-//                  run++ ) {
-//            for( int i = 0;
-//                     i < inp.length();
-//                     i++ ) {
-//                System.out.println(inp.substring(i,(i+run)));
-//                if (run==(inp.length))
-//                {
-//                    break;
-//                }
-//             }
-//            if (run==(inp.length))
-//                {
-//                    break;
-//                }
-//         }
+        //         for( int run = 1;
+        //                  run < inp.length();
+        //                  run++ ) {
+        //            for( int i = 0;
+        //                     i < inp.length();
+        //                     i++ ) {
+        //                System.out.println(inp.substring(i,(i+run)));
+        //         }
     }
 }
