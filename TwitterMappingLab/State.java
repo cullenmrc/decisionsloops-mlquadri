b323c165cebc784d09e454fe8700483b7129139b
mlquadri <mlquadri@stu.naperville203.org>
2016-11-10 11:27:07 -0600

    worked on lab

---
+++ TwitterMappingLab/State.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * Write a description of class State here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class State
+{
+    String abbreviation; 
+    twitter4j.GeoLocation center= new twitter4j.GeoLocation();
+    double area;
+    double sentiment;
+    /**
+     * Default constructor for objects of class State
+     */
+    public State()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
--- TwitterMappingLab/Tweet.java
+++ TwitterMappingLab/Tweet.java
@@ -1,5 +1,13 @@
-
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import twitter4j.*;
 /**
  * Write a description of class Tweet here.
  * 
@@ -11,22 +19,22 @@ public class Tweet
     /** description of instance variable x (add comment for each instance variable) */
     String user;
     String text;
-    twitter4j.GeoLocation location= new twitter4j.GeoLocation();
+    twitter4j.GeoLocation location;
     Date date;
     double sentiment;
+    Scanner s = new Scanner(System.in);
     /**
      * Default constructor for objects of class Tweet
      */
-    public Tweet(String user, String text, String location, String date)
+    public Tweet(String user, String text, twitter4j.GeoLocation location, Date date)
     {
         this.user=user;
-        this.text=text;
         this.location=location;
         this.date=date;
-        calculateSentiment duct = new calculateSentiment.getSengleton();
-        x = 0;
+        this.text=s.next();
+        calculateSentiment();
     }
-    
+    
     /**
      * An example of a method - replace this comment with your own
      *  that describes the operation of the method
@@ -38,9 +46,31 @@ public class Tweet
      * @param   y   description of parameter y
      * @return  description of the return value
      */
-    public int sampleMethod(int y)
+    public void calculateSentiment()
     {
-        
+        SentimentDictionary duct = SentimentDictionary.getSingleton();
+        Scanner s = new Scanner(text);
+        while(s.hasNext())
+        {
+            textword=s.next();
+            sentiment+=duct.getSentiment(textword);
+        }
     }
-
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String toString ()
+    {
+       String str="";
+       return str;
+    }
 }
